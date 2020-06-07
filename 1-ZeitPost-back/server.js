cda3aa9b17742e1f5b62338ccc09d4aa7c23c25a
Alfa Limonada <30637998+Alfa-limonada@users.noreply.github.com>
2020-06-07 16:47:38 -0500

    npm init y express

--- 1-ZeitPost-back/server.js
+++ 1-ZeitPost-back/server.js
@@ -1,4 +1,20 @@
 /*server.js - Acá se inicializa el backend 
-se incluyen módulos (require), 
-se inicializa express (framework de node para facilitar lógica)*/
+npm init en terminal para llenar manualmente la info del package.json
+npm init -y lo hace automático.*/
 
+//Primero ejecutar npm install express
+
+//--------
+const express = require('express');
+const app = express();
+
+app.get('/ping', (req, res) => {
+    res.status(200).end()    
+});
+
+
+
+//-------- This app starts a server and listens on port 3000 for connections --
+app.listen(3000, () => {
+    console.log("El puerto está funcionando")
+});
